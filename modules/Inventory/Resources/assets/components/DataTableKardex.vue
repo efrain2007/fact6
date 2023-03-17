7437a64d9d94179ff5cfc161347e1a66596f1eb4
Deyvis <soporte@buho.la>
2023-03-17 15:42:43 -0500

    Note infomative in kardex download button

--- modules/Inventory/Resources/assets/components/DataTableKardex.vue
+++ modules/Inventory/Resources/assets/components/DataTableKardex.vue
@@ -63,12 +63,14 @@
                                :loading="loading_submit" icon="el-icon-search">Buscar
                     </el-button>
                     <template v-if="records.length>0">
+
                         <el-button class="submit" type="danger" icon="el-icon-tickets"
                                    @click.prevent="clickDownload('pdf')">Exportar PDF
                         </el-button>
                         <el-button class="submit" type="success" @click.prevent="clickDownload('excel')"><i
                             class="fa fa-file-excel"></i> Exportar Excel
                         </el-button>
+                        <p class="text-danger"><b>NOTA:</b> Las descarga de los reportes corresponden a su almacén asignado.</p>
                     </template>
                 </div>
             </div>
