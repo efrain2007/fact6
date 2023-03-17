b70e6a3b32e86644a691d4ad9ad7e8909b17b0db
Deyvis <soporte@buho.la>
2023-03-17 09:30:13 -0500

    HIdden excel cash report POS

--- resources/js/views/tenant/cash/index.vue
+++ resources/js/views/tenant/cash/index.vue
@@ -77,13 +77,13 @@
                                 <button type="button" class="btn waves-effect waves-light btn-xs btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Reporte Efectivo <span class="caret"></span></button>
                                 <div class="dropdown-menu" role="menu" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 42px, 0px);">
                                     <!-- <a class="dropdown-item text-1" href="#" @click.prevent="clickDownloadProducts(row.id, 'pdf')">PDF</a> -->
-                                    <a class="dropdown-item text-1" href="#" @click.prevent="clickDownloadReportCash(row.id, 'excel')">Excel</a>
+                                    <!-- <a class="dropdown-item text-1" href="#" @click.prevent="clickDownloadReportCash(row.id, 'excel')">Excel</a> -->
                                     
                                     <el-tooltip class="item"
                                                 content="Ingresos en efectivo con destino caja"
                                                 effect="dark"
                                                 placement="right-end">
-                                        <a class="dropdown-item text-1" href="#" @click.prevent="clickCashPaymentReportExcel(row.id)">Excel V2</a>
+                                        <a class="dropdown-item text-1" href="#" @click.prevent="clickCashPaymentReportExcel(row.id)">Excel</a>
                                     </el-tooltip>
 
                                     <a class="dropdown-item text-1" href="#" @click.prevent="clickDownloadReportIncomeEgress(row.id)">Ingresos y egresos</a>
