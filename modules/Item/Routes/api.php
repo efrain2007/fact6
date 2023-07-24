645508e27cd6f7b233881ea242e907f6cddc6efc
Deyvis <soporte@buho.la>
2023-07-24 09:59:22 -0500

    Update: api route item sync woocommerce

--- modules/Item/Routes/api.php
+++ modules/Item/Routes/api.php
@@ -2,18 +2,19 @@
 
 $hostname = app(Hyn\Tenancy\Contracts\CurrentHostname::class);
 
-if ($hostname) 
+if ($hostname)
 {
     Route::domain($hostname->fqdn)->group(function () {
 
         Route::middleware(['auth:api', 'locked.tenant'])->group(function () {
 
             Route::prefix('items')->group(function () {
-                
+
                 // Route::post('update', 'Api\ItemController@update');
+                Route::get('export/stock', 'ItemController@ItemExportStock');
 
             });
 
-        }); 
+        });
     });
-} 
+}
