edf0e39c5e157f9b03789fa77f9614803d6ff762
rash07 <sh.raul.jgb@gmail.com>
2024-01-24 16:01:59 -0500

    fix | ajuste de impresion de nombre en xml para facturas y boletas

--- app/CoreFacturalo/Templates/xml/invoice.blade.php
+++ app/CoreFacturalo/Templates/xml/invoice.blade.php
@@ -4,10 +4,10 @@
     $customer = $document->customer;
 
     $document_xml_service = new Modules\Document\Services\DocumentXmlService;
-    
+
     // Cargos globales que no afectan la base imponible del IGV/IVAP
     $tot_charges = $document_xml_service->getGlobalChargesNoBase($document);
-   
+
     //descuento global - item que no afectan la base imponible
     $total_discount_no_base = $document_xml_service->getGlobalDiscountsNoBase($document) + $document_xml_service->getItemsDiscountsNoBase($document);
 
@@ -97,7 +97,7 @@
                 <cbc:Name><![CDATA[{{ $company->trade_name }}]]></cbc:Name>
             </cac:PartyName>
             <cac:PartyLegalEntity>
-                <cbc:RegistrationName><![CDATA[{{!! $company->name !!}}]]></cbc:RegistrationName>
+                <cbc:RegistrationName><![CDATA[{{ $company->name }}]]></cbc:RegistrationName>
                 <cac:RegistrationAddress>
                     <cbc:ID>{{ $establishment->district_id }}</cbc:ID>
                     <cbc:AddressTypeCode>{{ $establishment->code }}</cbc:AddressTypeCode>
